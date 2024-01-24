87e585f2a479dc80bc845bf2da6a2277334df3fb
erikvw <ew2789@gmail.com>
2024-01-23 23:36:17 -0600

    fix tests

--- microbiology_app/models.py
+++ microbiology_app/models.py
@@ -14,6 +14,8 @@ from edc_utils import get_utcnow
 from edc_visit_schedule.model_mixins import OffScheduleModelMixin, OnScheduleModelMixin
 from edc_visit_tracking.models import SubjectVisit
 
+from microbiology_app.consents import consent_v1
+
 
 class SubjectConsent(
     SiteModelMixin,
@@ -36,6 +38,7 @@ class SubjectConsent(
 
 
 class SubjectScreening(ScreeningModelMixin, BaseUuidModel):
+    consent_definition = consent_v1
     objects = SubjectIdentifierManager()
 
 
