90dfa812c6bf150e8b0005956e4a6493cea442a5
erikvw <ew2789@gmail.com>
2024-03-08 00:11:47 -0600

    fix tests

--- microbiology_app/consents.py
+++ microbiology_app/consents.py
@@ -3,7 +3,7 @@ from edc_constants.constants import FEMALE, MALE
 from edc_protocol.research_protocol_config import ResearchProtocolConfig
 
 consent_v1 = ConsentDefinition(
-    "microbiology_app.subjectconsent",
+    "microbiology_app.subjectconsentv1",
     version="1",
     start=ResearchProtocolConfig().study_open_datetime,
     end=ResearchProtocolConfig().study_close_datetime,
