d9805ab4718c50ef1ba969e983c5f795d14d8976
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2024-04-18 11:29:11 +0100

    Add verbose_name to AppConfig

--- edc_microbiology/apps.py
+++ edc_microbiology/apps.py
@@ -3,5 +3,6 @@ from django.apps import AppConfig as DjangoAppConfig
 
 class AppConfig(DjangoAppConfig):
     name = "edc_microbiology"
+    verbose_name = "Edc Microbiology"
     has_exportable_data = True
     include_in_administration_section = True
